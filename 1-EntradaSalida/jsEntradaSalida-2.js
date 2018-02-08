54d9a4645f51d89e8f95cf2f199333ce285e31b8
duarte1999 <36281606+duarte1999@users.noreply.github.com>
2018-02-08 20:49:17 -0300

    ejercicio 1 y 2

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,23 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
+// defino una variable
+    var nombre;
+    alert(nombre);
+    nombre=prompt("matias","full");
+    alert(nombre);
+
+
+
+
+
+    /*
+    nombre="matias";
+    alert(nombre);
+    alert("nombre");
+    */
+    // var person = prompt("Please enter your name", "Harry Potter");
+
 	
 }
 
