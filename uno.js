780928e647f096cd7fd34be56abde933a3447ba2
duarte1999 <36281606+duarte1999@users.noreply.github.com>
2018-02-09 21:27:52 -0300

    Add files via upload

---
+++ uno.js
@@ -0,0 +1,20 @@
+
+function Mostrar()
+{
+    var laBase;
+    var perimetro;
+    laBase=document.getElementById("laBase").value;
+    perimetro=document.getElementById('perimetro').value;
+
+    laBase=parseInt(laBase);
+    perimetro=parseInt(perimetro);
+
+
+    alert("laBase" + perimetro);
+
+
+
+
+
+
+    
